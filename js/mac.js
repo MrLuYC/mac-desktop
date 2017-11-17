a0b2a805a4c524d63058a319d134b1d2c17837b9
JC-Lu <1057243829@qq.com>
2017-11-17 20:21:56 +0800

    添加Layer框架

---
+++ js/mac.js
@@ -0,0 +1,21 @@
+/**
+ * Created by 卢耀川 on 2017/11/17.
+ */
+
+window.Mac = {
+  _version: 'v0.1',
+  _debug: true,
+  _countTask: 0,
+  _newMsgCount: 0,
+  _iframeOnClick: {
+
+  },
+  
+  _init: function () {
+    
+  }
+};
+
+$(document).ready(function () {
+  Mac._init();
+});
