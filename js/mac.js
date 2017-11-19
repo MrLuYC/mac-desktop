e3e5fed43d469a1d50646395c037e83bc899372c
JC-Lu <1057243829@qq.com>
2017-11-19 22:06:47 +0800

    添加窗体

--- js/mac.js
+++ js/mac.js
@@ -2,20 +2,59 @@
  * Created by 卢耀川 on 2017/11/17.
  */
 
-window.Mac = {
-  _version: 'v0.1',
-  _debug: true,
-  _countTask: 0,
-  _newMsgCount: 0,
-  _iframeOnClick: {
-
-  },
-
-  _init: function () {
-
+Vue.component('mac-window',{
+  template: '<div class="mac-window" v-bind:style="item.style" v-on:mousedown="mousedown"> <div class="mac-window-header"> <div class="mac-widnow-header-btns"> <button class="header-btn header-btn-close"></button> <button class="header-btn header-btn-min"></button> <button class="header-btn header-btn-max"></button> </div> <div class="mac-window-header-title"> <a href="#">{{ item.title }}</a> </div> </div> </div>',
+  props: ['item'],
+  methods:{
+    mousedown: function (e) {
+      this.item.selected = true;
+    }
   }
-};
+});
 
-$(document).ready(function(){
-  Mac._init();
+new Vue({
+  el: '#mac',
+  data: {
+    winList: [
+      {
+        title: 'Safari',
+        offsetX: 0,
+        offsetY: 0,
+        w: 400,
+        h: 300,
+        style: {
+          width: '400px',
+          height: '300px',
+          left: '550px',
+          top: '200px'
+        },
+        selected: false
+      }
+    ]
+  },
+  methods: {
+    mousemove: function (e) {
+
+      // var list = this.winList;
+
+      // for (i = 0 ; i < list.length; i ++) {
+      //   if (list[i].selected) {
+      //     list[i].offsetX = e.pageX - list[i].offsetX;
+      //     list[i].offsetY = e.pageY - list[i].offsetY;
+      //     list[i].style.top = list[i].offsetY + 'px';
+      //     list[i].style.left = list[i].offsetX + 'px';
+      //   }
+      // }
+    },
+
+    mouseup: function (e) {
+      // var list = this.winList;
+      //
+      // for (i = 0 ; i < list.length; i ++) {
+      //   if (list[i].selected) {
+      //     list[i].selected = false;
+      //   }
+      // }
+    }
+  }
 });
